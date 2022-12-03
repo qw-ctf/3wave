e671be3cbdf64a98892441d911e9cb01073f9ff9
Daniel Svensson <dsvensson@gmail.com>
2022-12-03 17:06:21 +0100

    3wave 2.30.

---
+++ maps/dobsp.bat
@@ -0,0 +1,16 @@
+qbsp -onlyents e1m1.ent e1m1.bsp
+qbsp -onlyents e1m2.ent e1m2.bsp
+qbsp -onlyents e1m3.ent e1m3.bsp
+qbsp -onlyents e1m4.ent e1m4.bsp
+qbsp -onlyents e1m5.ent e1m5.bsp
+qbsp -onlyents e1m6.ent e1m6.bsp
+qbsp -onlyents e1m8.ent e1m8.bsp
+qbsp -onlyents e2m1.ent e2m1.bsp
+qbsp -onlyents e2m2.ent e2m2.bsp
+qbsp -onlyents e2m3.ent e2m3.bsp
+qbsp -onlyents e4m3.ent e4m3.bsp
+
+@ECHO OFF
+echo This .BAT created by ^SporK^
+echo                   spork@sonic.net
+
